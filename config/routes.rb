46eca8f214562215b5491b41d6e86845ede28bdd
nozomi.handa <parsley1102@gmail.com>
2020-06-28 02:42:41 +0000

    for heroku2

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get 'users/new'
   get 'users/create'
   root to: "toppages#index"
+  
   get "signup", to: "users#new"
   resources :users, only: [:index, :show, :new, :create]
   
